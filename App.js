1f562f7e3d3407e24e04807d7fed454cae7dec34
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-11-07 16:58:05 +0200

    added all components and info

---
+++ App.js
@@ -0,0 +1,5 @@
+// create a function to copy phone number or email to clipboard
+
+// create a function to display modal on project click and to remove modal on clicking outside of modal
+
+// add fucntion to download cv
